0812b192fcb2e9f783f10a80f0726c78115cfd4a
bata <batorgilerdenbold@gmail.com>
2024-02-19 00:04:13 +0800

    last

--- components/Modal.js
+++ components/Modal.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import "./modal.css"; // Make sure the path to your CSS file is correct
+import "./modal.css"; // Ensure this path is correct.
 
 const Modal = ({ show, onClose, children }) => {
   const modalRef = useRef(null);
@@ -11,10 +11,10 @@ const Modal = ({ show, onClose, children }) => {
       }
     };
 
-    window.addEventListener("keydown", handleEsc);
+    document.addEventListener("keydown", handleEsc);
 
     return () => {
-      window.removeEventListener("keydown", handleEsc);
+      document.removeEventListener("keydown", handleEsc);
     };
   }, [onClose]);
 
@@ -27,14 +27,14 @@ const Modal = ({ show, onClose, children }) => {
   return (
     <div
       ref={modalRef}
-      className={`modal-background${show ? " show" : ""}`}
+      className={`modal-background ${show ? "show" : ""}`}
       onClick={handleBackgroundClick}
     >
       <div className="modal-content">
         <button
           className="close-button"
           onClick={(e) => {
-            e.stopPropagation(); // Prevents click from propagating to modal-background
+            e.stopPropagation();
             onClose();
           }}
         >
