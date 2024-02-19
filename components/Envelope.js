f4f4d5c8a406ca2c62dcac6fdc30b8b4c4e2b45e
bata <batorgilerdenbold@gmail.com>
2024-02-19 10:37:15 +0800

    a

--- components/Envelope.js
+++ components/Envelope.js
@@ -1,7 +1,10 @@
 // components/Envelope.js
 const Envelope = ({ onClick }) => {
   return (
-    <div className="mb-8 text-9xl cursor-pointer mr-8" onClick={onClick}>
+    <div
+      className="mb-8 text-9xl cursor-pointer mr-8 flex justify-center items-center"
+      onClick={onClick}
+    >
       ✉️
     </div>
   );
